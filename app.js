c6f7b966627ee94c4331900038652bda215c8cc2
Matthew Ng <matthewmng1@gmail.com>
2023-06-22 23:26:50 +0100

    update app.js and server.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require("express");
+const path = require('path')
 const cors = require("cors");
 
 const { NotFoundError } = require("./expressError");
@@ -11,8 +12,10 @@ const usersRoutes = require("./routes/users");
 const steamRoutes = require("./routes/steamApps");
 
 const app = express();
+const buildPath = path.join(__dirname, 'build')
 
 app.use(cors());
+app.use(express.static(buildPath))
 app.use(express.json());
 app.use(authenticateJWT);
 
@@ -42,6 +45,10 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'))
+})
 
 
 module.exports = app;
