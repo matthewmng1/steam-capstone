9d8902cc4519fd2f877dc85454b7ad2ac83b9505
Matthew Ng <matthewmng1@gmail.com>
2023-06-22 23:57:43 +0100

    update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-const PORT = +process.env.PORT || 3000;
+const PORT = +process.env.PORT || 3001;
 
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
